dd9eb323eca0fb43cffbcfec7769765a6358860d
itopa <itopa@live.ca>
2016-11-14 17:47:21 -0800

    name change

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import re, ast
 
 # get version from __version__ variable in app's/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-with open('civil_contracting/__init__.py', 'rb') as f:
+with open('contracting/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
 
 setup(
-    name='civil_contracting',
+    name='contracting',
     version=version,
-    description='App for Civil Contracting Enterprises',
+    description='App for Contracting Enterprises',
     author='Revant Nandgaonkar',
     author_email='revant.one@gmail.com',
     packages=find_packages(),
